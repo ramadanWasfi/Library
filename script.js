c4d3615371ca9d9272304317392388019a0b6030
Ramadan Wasfi <ramadanwasfi2@gmail.com>
2022-09-08 22:37:11 +0200

    Add createBookCard and append functions
    created function to dynamically creates book card with book info
    created function to add the created book to my dom tree inside library div

--- script.js
+++ script.js
@@ -7,18 +7,81 @@ function Book (author, title, numOfPages) {
 }
 
 const addBookToLibrary = (newBook) => {
-    for (book in myLibrary) {
-        if(book.title === newBook.title)
-            alert('There is a book with the name you try to add');
-            return;
-    }
     myLibrary[myLibrary.length] = newBook;
 }
+
+
+const createBookCard = (book) => {
+    const myBook = document.createElement('div');
+    myBook.classList.add('book');
+
+    const readStateLabel = document.createElement('label');
+    readStateLabel.textContent = 'read';
+
+    const readStateInput = document.createElement('input');
+    readStateInput.type = 'checkbox';
+    readStateInput.id = 'readState';
+
+    const readState = document.createElement('div');
+    readState.appendChild(readStateInput);
+    readState.appendChild(readStateLabel);
+
+    
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.type = 'button';
+    deleteBtn.textContent = 'X';
+
+    const cardHeader = document.createElement('div');
+    cardHeader.classList.add('cardHeader');
+
+    cardHeader.appendChild(readState);
+    cardHeader.appendChild(deleteBtn);
+
+
+    const title = document.createElement('p');
+    title.textContent = book.title;
+
+    const author = document.createElement('p');
+    author.textContent = book.author;
+
+    const numOfPages = document.createElement('p');
+    numOfPages.textContent = book.numOfPages;
+
+    myBook.appendChild(cardHeader);
+    myBook.appendChild(title);
+    myBook.appendChild(author);
+    myBook.appendChild(numOfPages);
+    return myBook;
+}
+
+const appendBookCardtoLibraryDiv = (bookCard) => {
+    const library = document.querySelector('.library');
+    library.appendChild(bookCard);
+}
+
 
 let book1 = new Book('Ramadan', 'Slow Down', 50);
 let book2 = new Book('Ramadan', 'Slow Down', 50);
 let book3 = new Book('Ramadan', 'Slow Down', 50);
 
+let card = createBookCard(book1);
+appendBookCardtoLibraryDiv(card);
+
+let card2 = createBookCard(book1);
+appendBookCardtoLibraryDiv(card2);
+
+let card3 = createBookCard(book1);
+appendBookCardtoLibraryDiv(card3);
+
+let card4 = createBookCard(book1);
+appendBookCardtoLibraryDiv(card4);
+
+let card5 = createBookCard(book1);
+appendBookCardtoLibraryDiv(card5);
+
+let card6 = createBookCard(book1);
+appendBookCardtoLibraryDiv(card6);
 
 console.log(book1);
 
