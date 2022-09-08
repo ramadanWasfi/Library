aa5080180f3e4def11dcf285347602c36ac81376
Ramadan Wasfi <ramadanwasfi2@gmail.com>
2022-09-08 21:12:12 +0200

    Update addBookToLibrary function
    book name validation is added to preve

--- script.js
+++ script.js
@@ -7,13 +7,24 @@ function Book (author, title, numOfPages) {
 }
 
 const addBookToLibrary = (newBook) => {
+    for (book in myLibrary) {
+        if(book.title === newBook.title)
+            alert('There is a book with the name you try to add');
+            return;
+    }
     myLibrary[myLibrary.length] = newBook;
 }
 
 let book1 = new Book('Ramadan', 'Slow Down', 50);
+let book2 = new Book('Ramadan', 'Slow Down', 50);
+let book3 = new Book('Ramadan', 'Slow Down', 50);
+
 
 console.log(book1);
 
 addBookToLibrary(book1);
+addBookToLibrary(book2);
+addBookToLibrary(book3);
+
 
 console.log(myLibrary);
